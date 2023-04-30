dba654267dc52eebb38acadda2ff9d2052fca062
ShriGanesh Saini <67692684+rvsaini8107@users.noreply.github.com>
2023-04-30 12:02:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 function fibonacci(num) {
 // your code here
+	if(num==0)return 0;
+	if(num==1)return 1;
 	var sum=0; var num1=0;var num2=1;
-	for(let i=0;i<=num;i++)
+	for(let i=2;i<=num;i++)
 		{
 			sum=num1+num2;
 			num2=num1;
